8062668a4e9295df3dd45ee631f89295ec0efc35
Abdullah.Alajlan <abdullah.alajlan@outlook.com>
2019-12-08 15:51:04 +0300

    refactor: Removing some unwanted lines and adding comments

--- Guessing game.py
+++ Guessing game.py
@@ -1,14 +1,18 @@
-from random import randint 
+from random import randint
+# to generate a number between 1 & 9
 number = randint(1,9)
-print(number)
+# to find the number of guesses after the game is done
 guesses = []
+# just to initiate the variable
 guess = 0
+# loop to maintain the user untill correct guess is provided
 while number != guess:
-    guess = input('Guess a number, or type "exit" to end the game\n')
+    guess = int(input('Guess a number, or type "exit" to end the game\n'))
+    # to give the user an option to end the game if he doesn't want to play
     if guess == 'exit':
         break
-    guess = int(guess)
     guesses.append(guess)
+    # to let the user to go high, low or if he won
     if guess > number:
         print('your guess is too high !!')
     elif guess < number:
