ddffe3d1785d4a19532edb6a78387d8a901f8ab7
Abdullah.Alajlan <abdullah.alajlan@outlook.com>
2019-12-12 19:37:39 +0300

    docs: Writing the comments of the print_backward.py code

--- print_backward.py
+++ print_backward.py
@@ -1,9 +1,11 @@
+# this function is used to return the sentence written backwards
 def print_in_backward(sentence):
+    #split the sentence words and save them on a list
     sentence_splitted = sentence.split(' ')
     backward_sentence_splitted = []
     for i in range(1,len(sentence_splitted)+1):
-        
-        backward_sentence_splitted.append(sentence_splitted[-i])
+
+    backward_sentence_splitted.append(sentence_splitted[-i])
     backward_sentence = ' '.join(backward_sentence_splitted)
     print(backward_sentence)
     return backward_sentence
